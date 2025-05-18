190da97aa62110a808fc4a6dfb998a73278587d1
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-05-18 18:12:35 +0200

    remove dummy test

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -3,12 +3,6 @@ import pytest
 import sif2jax
 
 
-# Dummy test to get started
-def test_rosenbrock():
-    problem = pycutest.import_problem("ROSENBR")
-    assert problem is not None
-
-
 @pytest.mark.parametrize("problem", sif2jax.problems)
 def test_correct_name(problem):
     pycutest_problem = pycutest.import_problem(problem.name())

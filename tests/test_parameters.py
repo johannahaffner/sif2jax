287bee38eb3e93cc6feaa6fe5511fb425220e320
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-05-18 18:11:01 +0200

    remove support for variably-dimensioned problems, since we cannot test these against pycutest

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -13,3 +13,10 @@ def test_rosenbrock():
 def test_correct_name(problem):
     pycutest_problem = pycutest.import_problem(problem.name())
     assert pycutest_problem is not None
+
+
+@pytest.mark.parametrize("problem", sif2jax.problems)
+def test_correct_dimension(problem):
+    pycutest_problem = pycutest.import_problem(problem.name())
+    dimensions = problem.y0().size
+    assert dimensions == pycutest_problem.n

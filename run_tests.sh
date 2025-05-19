531bb98dff3caaadde0be8058f6e802a24ad8265
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-05-19 21:27:11 +0200

    create script to run tests in container

---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Configuration
+CONTAINER_IMAGE="johannahaffner/pycutest:latest"
+MOUNT_PATH="/workspace"  # Path inside container where your code will be mounted
+LOCAL_PATH=$(pwd)        # Current directory with your code
+
+# Run tests in container
+docker run -it --rm \
+  -v ${LOCAL_PATH}:${MOUNT_PATH} \
+  -w ${MOUNT_PATH} \
+  ${CONTAINER_IMAGE} \
+  bash -c "pip install -e . && pytest $@"

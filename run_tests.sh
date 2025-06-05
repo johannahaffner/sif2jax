8c6eaef0bbf3d1b72b7c9cb99f576ee4540cc6fb
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-06-05 10:22:40 +0000

    make test noninteractive and patch through pytest options

--- run_tests.sh
+++ run_tests.sh
@@ -3,11 +3,11 @@
 # Configuration
 CONTAINER_IMAGE="johannahaffner/pycutest:latest"
 MOUNT_PATH="/workspace"  # Path inside container where your code will be mounted
-LOCAL_PATH=$(pwd)        # Current directory with your code
+LOCAL_PATH=/Users/jhaffner/Desktop/sif2jax
 
 # Run tests in container
-docker run -it --rm \
+docker run --rm \
   -v ${LOCAL_PATH}:${MOUNT_PATH} \
   -w ${MOUNT_PATH} \
   ${CONTAINER_IMAGE} \
-  bash -c "pip install -e . && pytest $@"
+  bash -c "pip install -e . && pytest \"\$@\"" -- "$@"

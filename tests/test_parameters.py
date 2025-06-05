98bd00a59c929fed56fb029378e0d1d48191b2ad
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-06-05 16:49:20 +0000

    extra tests and removal of unimplemented problems, skipping jit test

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,7 +1,9 @@
 import jax
+import jax.flatten_util as jfu
 import jax.numpy as jnp
 import numpy as np
-import pycutest
+import pycutest  # pyright: ignore[reportMissingImports]  - test runs in container
+import pytest  # pyright: ignore[reportMissingImports]  - test runs in container
 import sif2jax
 
 
@@ -38,39 +40,52 @@ def test_correct_dimension(problem):
 
 def test_correct_starting_value(problem):
     pycutest_problem = pycutest.import_problem(problem.name())
-    assert np.allclose(pycutest_problem.x0, problem.y0(), rtol=1e-8, atol=1e-8)
+    assert np.allclose(pycutest_problem.x0, problem.y0())
 
 
 def test_correct_objective_at_start(problem):
     pycutest_problem = pycutest.import_problem(problem.name())
     pycutest_value = pycutest_problem.obj(pycutest_problem.x0)
     sif2jax_value = problem.objective(problem.y0(), problem.args())
-    assert np.allclose(pycutest_value, sif2jax_value, rtol=1e-6, atol=1e-6)
+    assert np.allclose(pycutest_value, sif2jax_value)
 
 
 def test_correct_gradient_at_start(problem):
     pycutest_problem = pycutest.import_problem(problem.name())
     pycutest_gradient = pycutest_problem.grad(pycutest_problem.x0)
     sif2jax_gradient = jax.grad(problem.objective)(problem.y0(), problem.args())
-    assert np.allclose(pycutest_gradient, sif2jax_gradient, rtol=1e-8, atol=1e-8)
+    assert np.allclose(pycutest_gradient, sif2jax_gradient)
 
 
 def test_correct_hessian_at_start(problem):
-    pycutest_problem = pycutest.import_problem(problem.name())
-    pycutest_hessian = pycutest_problem.ihess(pycutest_problem.x0)
-    sif2jax_hessian = jax.hessian(problem.objective)(problem.y0(), problem.args())
-    assert np.allclose(pycutest_hessian, sif2jax_hessian, rtol=1e-8, atol=1e-8)
+    if problem.num_variables < 1000:
+        pycutest_problem = pycutest.import_problem(problem.name())
+        pycutest_hessian = pycutest_problem.ihess(pycutest_problem.x0)
+        sif2jax_hessian = jax.hessian(problem.objective)(problem.y0(), problem.args())
+        assert np.allclose(pycutest_hessian, sif2jax_hessian)
+    else:
+        pass  # Skip Hessian test for large problems to save time
 
 
 def test_correct_constraints_at_start(problem):
     if isinstance(problem, sif2jax.AbstractConstrainedMinimisation):
         pycutest_problem = pycutest.import_problem(problem.name())
+        assert pycutest_problem.m > 0, "Problem should have constraints."
+
         pycutest_constraints = pycutest_problem.cons(pycutest_problem.x0)
-        print(f"pycutest constraints: {pycutest_constraints}")
-        # sif2jax_constraints = problem.constraints(problem.y0(), problem.args())
-        # assert np.allclose(pycutest_constraints, sif2jax_constraints)
-        # TODO choose appropriate tolerance
-        pass
+        pycutest_equalities = pycutest_constraints[pycutest_problem.is_eq_cons]  # pyright: ignore
+        pycutest_equalities = jnp.array(pycutest_equalities).squeeze()
+        pycutest_inequalities = pycutest_constraints[~pycutest_problem.is_eq_cons]  # pyright: ignore
+        pycutest_inequalities = jnp.array(pycutest_inequalities).squeeze()
+
+        sif2jax_constraints = problem.constraint(problem.y0())
+        sif2jax_equalities, sif2jax_inequalities = sif2jax_constraints
+        sif2jax_equalities, _ = jfu.ravel_pytree(sif2jax_equalities)
+        sif2jax_inequalities, _ = jfu.ravel_pytree(sif2jax_inequalities)
+
+        # Check that the constraints match
+        assert np.allclose(pycutest_equalities, sif2jax_equalities)
+        assert np.allclose(pycutest_inequalities, sif2jax_inequalities)
     else:
         pass
 
@@ -78,6 +93,7 @@ def test_correct_constraints_at_start(problem):
 # test correct constraint jacobian at start
 
 
+@pytest.mark.skip(reason="can slow down tests for expensive problems, needs fix.")
 def test_compilation(problem):
     try:
         compiled = jax.jit(problem.objective)

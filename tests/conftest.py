bdc735645499a68e0803b7a7eebf45514b985986
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-06-20 14:16:45 +0000

    strict dtype and shape promotion

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,8 @@ import jax
 
 
 jax.config.update("jax_enable_x64", True)
-# jax.config.update("jax_numpy_rank_promotion", "raise")
-jax.config.update("jax_numpy_dtype_promotion", "standard")
+jax.config.update("jax_numpy_rank_promotion", "raise")
+jax.config.update("jax_numpy_dtype_promotion", "strict")
 
 
 def pytest_addoption(parser):

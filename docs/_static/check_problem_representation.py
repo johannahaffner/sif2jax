c0ca81fc73afa443ccb60653abf5755ddcb7afc5
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-08-01 20:38:43 +0000

    add stub for histograms of problem dimensionality

---
+++ docs/_static/check_problem_representation.py
@@ -0,0 +1,6 @@
+# TODO(claude): Write a script that accesses pycutest and sif2jax and retrieve the
+# (default) dimension of the problems. Then create a figure with histograms for each
+# library (plot them on top of each other, with alpha=0.5 or so).
+# Use matplotlib for the plots.
+# Also write a script that runs this in the container (take inspiration from
+# run_tests.sh to see how this is done for this docker-in-docker setup).
